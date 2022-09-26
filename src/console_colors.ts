d4d6b57791e63e5e757eabd4e8b9947326d5a616
Dani <dani.93.perez@gmail.com>
2022-09-26 08:32:07 +0200

    Add more colors to console

--- src/console_colors.ts
+++ src/console_colors.ts
@@ -1,6 +1,10 @@
 export enum TextColors {
     Green = "\x1b[32m",
-    Red = "\x1b[31m"
+    Red = "\x1b[31m",
+    Yellow = "\x1b[33m",
+    Blue = "\x1b[34m",
+    Magenta = "\x1b[35m",
+    White = "\x1b[37m"
 }
 
 export const endLine = '\x1b[0m'
